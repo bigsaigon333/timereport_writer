18ed4ef848f6a10bc602cd5b45e7f0ed5283a341
bigsaigon333 <likepeppermint@gmail.com>
2020-08-10 23:25:33 +0900

    UI 전면수정 && js 일부 추가

--- timereport.js
+++ timereport.js
@@ -1,10 +1,10 @@
-let form = document.getElementById("inputForm");
-let submitButton = document.getElementById("submitButton");
-let projectCodeName = document.getElementById("projectCode");
-let startTime = document.getElementById("startTime");
-let endTime = document.getElementById("endTime");
-let timeReportList = document.getElementById("timeReportList");
-let tbody = document.getElementsByTagName("tbody")[0];
+const inputForm = document.querySelector("#inputForm");
+const main = document.querySelector(".main");
+const timeReportList = document.querySelector(".js-timereport-ul");
+
+const projectCodeInput = document.getElementById("projectCodeInput");
+const startTimeInput = document.getElementById("startTime");
+const endTimeInput = document.getElementById("endTime");
 
 const parseTime = (t) => {
 	console.log(t);
@@ -23,54 +23,72 @@ const calTimeDifference = (a, b) => {
 	if (diffInMin < 0) {
 		return -1;
 	} else {
-		let steppedMin = Math.round(parseInt(diffInMin % 60) / 15) * 15;
-		return { hour: parseInt(diffInMin / 60), min: steppedMin };
+		let hour = parseInt(diffInMin / 60);
+		let min = Math.round(parseInt(diffInMin % 60) / 15) * 15;
+
+		return { hour, min };
 	}
+};
+
+const printPeriod = (td) => {
+	return ("0" + td.hour).slice(-2) + "H" + " " + ("0" + td.min).slice(-2) + "M";
+};
+
+const initElements = (period) => {
+	const li = document.createElement("li");
+	const div = document.createElement("div");
+	const btnDiv = document.createElement("div");
+	const reviseBtn = document.createElement("button");
+	const deleteBtn = document.createElement("button");
+	const projectCodeSpan = document.createElement("span");
+	const startTimeSpan = document.createElement("span");
+	const endTimeSpan = document.createElement("span");
+	const periodSpan = document.createElement("span");
+
+	li.appendChild(div);
+	div.appendChild(btnDiv);
+	btnDiv.appendChild(reviseBtn);
+	btnDiv.appendChild(deleteBtn);
+	div.appendChild(projectCodeSpan);
+	div.appendChild(startTimeSpan);
+	div.appendChild(endTimeSpan);
+	div.appendChild(periodSpan);
+
+	li.classList.add("timereport__li");
+	div.classList.add("timereport-row");
+
+	reviseBtn.innerText = "*";
+	deleteBtn.innerText = "-";
+
+	projectCodeSpan.innerText = projectCodeInput.value;
+	startTimeSpan.innerText = startTimeInput.value;
+	endTimeSpan.innerText = endTimeInput.value;
+	periodSpan.innerText = period;
+	return li;
 };
 
 const writeTimeReport = (event) => {
+	// 0. prevent Default
 	event.preventDefault();
 
-	let timeDifference = calTimeDifference(startTime.value, endTime.value);
-	if (timeDifference === -1) {
+	// 1. validate
+	let period = calTimeDifference(startTimeInput.value, endTimeInput.value);
+	if (period === -1) {
 		alert("시작시간이 종료시간보다 늦습니다.");
-		startTime.focus();
+		startTimeInput.focus();
 		return;
 	}
+	console.log(period);
 
-	let tr = document.createElement("tr");
-	let tdProjectCodeName = document.createElement("td");
-	tdProjectCodeName.textContent = projectCodeName.value;
-	tr.append(tdProjectCodeName);
-
-	let tdStartTime = document.createElement("td");
-	tdStartTime.textContent = startTime.value;
-	tr.append(tdStartTime);
-
-	let tdEndTime = document.createElement("td");
-	tdEndTime.textContent = endTime.value;
-	tr.append(tdEndTime);
-
-	let tdTimeDiff = document.createElement("td");
-	tdTimeDiff.textContent =
-		("0" + timeDifference.hour).slice(-2) +
-		"H" +
-		" " +
-		("0" + timeDifference.min).slice(-2) +
-		"M";
-
-	tr.append(tdTimeDiff);
-
-	// let tdMinDiff = document.createElement("td");
-	// tdMinDiff.textContent = tr.append(tdMinDiff);
-
-	tbody.append(tr);
+	//2. write Time Difference on main
+	const li = initElements(printPeriod(period));
+	timeReportList.appendChild(li);
 
 	// Clear all the content value
-	projectCodeName.value = "";
-	startTime.value = "";
-	endTime.value = "";
-	projectCodeName.focus();
+	// projectCodeName.value = "";
+	// startTime.value = "";
+	// endTime.value = "";
+	// projectCodeName.focus();
 };
 
-form.addEventListener("submit", writeTimeReport);
+inputForm.addEventListener("submit", writeTimeReport);
