c17805bb8667a755a687f3dbf8e7777da83f9fa5
bigsaigon333 <likepeppermint@gmail.com>
2020-08-18 23:54:01 +0900

    localStorage에 저장기능 추가

--- timereport.js
+++ timereport.js
@@ -1,73 +1,12 @@
 const inputForm = document.querySelector("#inputForm");
 const main = document.querySelector(".main");
 const timeReportList = document.querySelector(".js-timereport-ul");
-
 const projectCodeInput = document.getElementById("projectCodeInput");
 const startTimeInput = document.getElementById("startTime");
 const endTimeInput = document.getElementById("endTime");
 
-const parseTime = (t) => {
-	console.log(t);
-	return { hour: t.slice(0, 2), min: t.slice(3, 5) };
-};
-
-const calTimeDifference = (a, b) => {
-	let time1 = parseTime(a);
-	let time2 = parseTime(b);
-
-	let date1 = new Date().setHours(time1.hour, time1.min);
-	let date2 = new Date().setHours(time2.hour, time2.min);
-
-	let diffInMin = parseInt((date2 - date1) / (1000 * 60));
-
-	if (diffInMin < 0) {
-		return -1;
-	} else {
-		let hour = parseInt(diffInMin / 60);
-		let min = Math.round(parseInt(diffInMin % 60) / 15) * 15;
-
-		return { hour, min };
-	}
-};
-
-const printPeriod = (td) => {
-	return ("0" + td.hour).slice(-2) + "H" + " " + ("0" + td.min).slice(-2) + "M";
-};
-
-const initElements = (period) => {
-	const li = document.createElement("li");
-	const div = document.createElement("div");
-	const btnDiv = document.createElement("div");
-	const reviseBtn = document.createElement("button");
-	const deleteBtn = document.createElement("button");
-	const projectCodeSpan = document.createElement("span");
-	const startTimeSpan = document.createElement("span");
-	const endTimeSpan = document.createElement("span");
-	const periodSpan = document.createElement("span");
-
-	li.appendChild(div);
-	div.appendChild(btnDiv);
-	btnDiv.appendChild(reviseBtn);
-	btnDiv.appendChild(deleteBtn);
-	div.appendChild(projectCodeSpan);
-	div.appendChild(startTimeSpan);
-	div.appendChild(endTimeSpan);
-	div.appendChild(periodSpan);
-
-	li.classList.add("timereport__li");
-	div.classList.add("timereport-row");
-
-	reviseBtn.innerText = "*";
-	deleteBtn.innerText = "-";
-
-	projectCodeSpan.innerText = projectCodeInput.value;
-	startTimeSpan.innerText = startTimeInput.value;
-	endTimeSpan.innerText = endTimeInput.value;
-	periodSpan.innerText = period;
-	return li;
-};
-
-const writeTimeReport = (event) => {
+let idCount = 0;
+const appendTimeReportItem = (event) => {
 	// 0. prevent Default
 	event.preventDefault();
 
@@ -78,17 +17,29 @@ const writeTimeReport = (event) => {
 		startTimeInput.focus();
 		return;
 	}
-	console.log(period);
 
-	//2. write Time Difference on main
-	const li = initElements(printPeriod(period));
+	// 2. create TimeReport Object
+	const timeReportItem = {
+		id: idCount++,
+		projectCodeName: projectCodeInput.value,
+		startTime: startTimeInput.value,
+		endTime: endTimeInput.value,
+		period: period,
+	};
+	localStorage.setItem(timeReportItem.id, JSON.stringify(timeReportItem));
+	console.log(timeReportItem);
+
+	// 3. create view
+	const li = createView(timeReportItem);
+
+	//4. append view to timeReportList
 	timeReportList.appendChild(li);
 
-	// Clear all the content value
-	// projectCodeName.value = "";
-	// startTime.value = "";
-	// endTime.value = "";
-	// projectCodeName.focus();
+	// 5. Clear all the content value
+	projectCodeInput.value = "";
+	startTimeInput.value = endTimeInput.value;
+	endTimeInput.value = "";
+	projectCodeInput.focus();
 };
 
-inputForm.addEventListener("submit", writeTimeReport);
+inputForm.addEventListener("submit", appendTimeReportItem);
---
+++ view.js
@@ -0,0 +1,48 @@
+const createView = (timeReportItem) => {
+	const li = document.createElement("li");
+	const div = document.createElement("div");
+	const btnDiv = document.createElement("div");
+	const reviseBtn = document.createElement("button");
+	const deleteBtn = document.createElement("button");
+	const projectCodeSpan = document.createElement("span");
+	const startTimeSpan = document.createElement("span");
+	const endTimeSpan = document.createElement("span");
+	const periodSpan = document.createElement("span");
+
+	const reviseBtnHandler = (event) => {
+		projectCodeInput.value = projectCodeSpan.innerText;
+		startTimeInput.value = startTimeSpan.innerText;
+		endTimeInput.value = endTimeSpan.innerText;
+		deleteBtnHandler(event);
+	};
+
+	const deleteBtnHandler = (event) => {
+		const deleteItemId = li.getAttribute("id");
+		console.log("[delete] li removed: ", deleteItemId);
+		localStorage.removeItem(deleteItemId);
+		li.remove();
+	};
+
+	li.setAttribute("id", timeReportItem.id);
+	li.appendChild(div);
+	div.appendChild(btnDiv);
+	btnDiv.appendChild(reviseBtn);
+	btnDiv.appendChild(deleteBtn);
+	div.appendChild(projectCodeSpan);
+	div.appendChild(startTimeSpan);
+	div.appendChild(endTimeSpan);
+	div.appendChild(periodSpan);
+
+	li.classList.add("timereport__li");
+	div.classList.add("timereport-row");
+
+	reviseBtn.innerText = "revise";
+	reviseBtn.addEventListener("click", reviseBtnHandler);
+	deleteBtn.innerText = "delete";
+	deleteBtn.addEventListener("click", deleteBtnHandler);
+	projectCodeSpan.innerText = projectCodeInput.value;
+	startTimeSpan.innerText = startTimeInput.value;
+	endTimeSpan.innerText = endTimeInput.value;
+	periodSpan.innerText = getFormattedPeriod(timeReportItem.period);
+	return li;
+};
